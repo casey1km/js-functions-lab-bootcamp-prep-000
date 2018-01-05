7becd3dc32b1902ebed4d93cca259a4c8da75038
Kateri Casey <casey1km@gmail.com>
2018-01-05 03:26:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,12 @@
 function happyHolidays () {
  console.log("Happy Holidays!")
 }
+function happyHolidaysTo(name) {
+  return `Happy Holidays, ${name}!`
+}
+function happyHolidayTo(holiday,name) {
+  return `Happy ${holiday}, ${name}!`
+}
+function holidayCountdown(holiday,number) {
+  return `It's ${days} days until ${holiday}!`
+}
